296b1757e20a4af23021a9739b75de14b33f3849
AKATSUKI_HEADER <132822705+glasshats@users.noreply.github.com>
2023-11-13 08:44:38 -0500

    Add files via upload

---
+++ loop17.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    for( i = 1;i<=5;i++){
+        for( j=i;j<5;j++){
+            printf(" ");
+
+        }for(j=1;j<=(2*i-1);j++)
+        {
+        if(i==5||j==1||j==(2*i-1)){
+            printf("*");
+        }
+        else{
+            printf(" ");
+        }
+        }
+        printf("\n");
+    }
+}
+/*
+    *
+   * *
+  *   *
+ *     *
+*********
+
+
+
+
+
+
+*/
