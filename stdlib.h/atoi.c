c27a7b45a0cd2aad03c4027d7fe6f9917b20343b
AKATSUKI_HEADER <132822705+glasshats@users.noreply.github.com>
2023-11-24 07:44:24 -0700

    Add files via upload

---
+++ stdlib.h/atoi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    char int_test[]= "78";
+    char float_test[]="90.45";
+    char long_test[]="898982309020802";
+    
+    int int_result = atoi(int_test);
+    double float_result=atof(float_test);
+    long int long_result=atol(long_test);
+
+    printf("int :  %d\n",int_result);
+    printf("float : %f\n",float_result);
+    printf("long : %ld\n",long_test);
+}
